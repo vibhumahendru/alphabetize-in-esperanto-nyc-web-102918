525779e2d0ee9ccc6a361052a3981e251ad690c1
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-18 18:29:20 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,12 +1,3 @@
 def alphabetize(arr)
-  esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
-  arr.sort_by do |word|
-    word.split('').collect do |letter|
-      esperanto_alphabet.index(letter)
-    end
-  end
+  arr.sort
 end
-
-arr = ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
-
-alphabetize(arr)
