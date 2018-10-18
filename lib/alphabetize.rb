e4baae30112342fe87875226c8a3cf44fc496ff4
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-18 18:26:36 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
 def alphabetize(arr)
-  # code here
+  array_Esperanto_letters = []
+  es_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  for x in 0...es_alphabet.length
+  array_Esperanto_letters.push(es_alphabet[x])
+end
+array_Esperanto_letters
+
+arr.sort_by do |element|
+  
+end
 end
