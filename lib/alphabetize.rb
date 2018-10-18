174f92e4b2dcb2d43ae6441422012b24aedb38f6
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-18 18:51:34 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,9 +1,8 @@
 def alphabetize(array)
    esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
-  
-  newAr = [] 
+
    array.sort_by do |element|
-    newAr << element.split('')
+    element.split('').collect |letter|
     newAr.each do |letter|
      esperanto_alphabet.index(letter)
    end
