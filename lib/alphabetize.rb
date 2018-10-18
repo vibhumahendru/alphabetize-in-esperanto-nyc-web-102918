84e4d389b102e8088be7e98180dd0f1e862e6346
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-18 18:48:07 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,5 @@
-def alphabetize(arr)
-  
+def alphabetize(array)
+   esperanto_alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+   
+   array.sort_by
 end
